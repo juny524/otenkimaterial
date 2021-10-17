366907ceefeec0f121ad07ba170c17027fc2127a
juny524 <juny5241@gmail.com>
2021-10-17 15:11:50 +0900

    Angular Materialを導入

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
